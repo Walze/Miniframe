acba07cd63101e04e5c215bf73c44b8b4ff8529f
Walze <catufuzgu@gmail.com>
2017-11-09 16:47:02 -0200

    .

--- dev_html/js/router.js
+++ dev_html/js/router.js
@@ -12,3 +12,22 @@ document.querySelectorAll('[route-href]').forEach(el => {
     window.location = link;
   });
 });
+
+var request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:3000/api', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+  } else {
+    // We reached our target server, but it returned an error
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./'));
-
-app.get('/', (req, res) => {
-  res.sendFile('./index.html');
+app.get('/api', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.json({
+    message: 'Welcome to the Server'
+  });
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, e => console.log(e));
