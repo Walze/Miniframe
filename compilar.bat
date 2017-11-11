164cfe18bdf85f66eba4d502ca6d01f5b935c25f
Walze <catufuzgu@gmail.com>
2017-11-11 01:41:57 -0200

    bat remove

--- compilar.bat
+++
@@ -1,5 +0,0 @@
-call npm run build
-xcopy /s "public_html\*" "\\TLP30\TREINAMENTO\02. Projetos Educacionais\3. Gestão EAD\3. TeleListas.net e Mobile\3. Material de Apoio\9. Storyboard\HTML"
-t
-pause
-
