811e3db7c9ee85f0f634d15571d6ce3be12e46ff
Yuri Renê Ferreira Rosino <yrosino@brtelecom.com.br>
2018-01-17 11:07:21 -0200

    major updates on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,123 +1,59 @@
-const HtmlWebpackPrefixPlugin = require('html-webpack-prefix-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const uglifyJS = require('uglifyjs-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
-
-const MODE = process.env.npm_lifecycle_event;
-
-let Configs = require('./config.json');
-MODE == 'build' ? (Configs.host = Configs.host) : (Configs.host = '');
-Configs.host = '';
-
-const clientConfig = {
-  entry: './src/js/main.js',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader'
+const
+  webpack = require('webpack'),
+  path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+  config = {
+    entry: {
+      main: './src/js/main.js',
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
           }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader'
-        }
-      }
-    ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, `docs/${Configs.projName}_files`),
-    publicPath: ''
-  },
-  devServer: {
-    publicPath: '/', //where it compiles
-    contentBase: './', //where index.html is
-    hot: true,
-    historyApiFallback: {
-      index: 'index.html'
+        },
+        {
+          test: /\.(woff|woff2|ttf|eot)$/,
+          loader: 'file-loader?name=files/[name].[ext]'
+        },
+        {
+          test: /\.(pdf|jpe?g|png|gif|svg)$/i,
+          loader: 'file-loader?name=files/[name].[ext]'
+        },
+      ]
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'docs/'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: './src/index.html', inject: false }),
+    ],
+    devServer: {
+      hot: true,
+      historyApiFallback: { index: 'index.html' }
     }
   }
-};
 
-//plugin declarations
-var extract = new ExtractTextPlugin('bundle.css');
-var htmlwebpack = new HtmlWebpackPlugin({
-  title: Configs.projName,
-  filename: '../index.html',
-  template: 'src/index.html',
-  prefix: `${Configs.host}${Configs.projName}_files/`,
-  minify: {
-    removeComments: true
-  }
-});
-var htmlwebpackprefix = new HtmlWebpackPrefixPlugin();
-var providePlugin = new webpack.ProvidePlugin({
-  //
-});
-
-var livereload = new LiveReloadPlugin();
-//end plugins declarations
-
-//insert plugins
-clientConfig.plugins = [
-  extract,
-  htmlwebpack,
-  htmlwebpackprefix,
-  providePlugin,
-  livereload
-];
-
-// mode selector
-if (MODE == 'build') {
-  clientConfig.output.path = path.resolve(
-    __dirname,
-    `docs/${Configs.projName}_files`
-  );
-  clientConfig.plugins.push(
-    new uglifyJS({
-      sourceMap: false,
-      uglifyOptions: {
-        output: {
-          comments: false,
-          beautify: false,
-          unused: true
-        },
-        compress: {
-          dead_code: true,
-          global_defs: { DEBUG: false }
-        }
-      }
-    })
-  );
-} else if (MODE == 'dev') {
-  clientConfig.output.path = path.resolve(__dirname, '');
-  htmlwebpack.options.prefix = '/';
-  htmlwebpack.options.filename = 'index.html';
-  clientConfig.module.rules[1].use[0].loader = `file-loader?name=${Configs.projName}_files/[name].[ext]`;
+if (process.env.NODE_ENV == 'production') {
+  config.plugins.push(new UglifyJsPlugin({
+    uglifyOptions: {
+      output: {
+        comments: false,
+        beautify: false,
+      },
+      warnings: false,
+    },
+    sourceMap: true
+  }))
 }
 
-module.exports = [clientConfig];
+module.exports = config
